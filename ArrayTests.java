86bbebcad220e55b1c62203d4838b2621e50f679
prsavla <prchheda@ucsd.edu>
2024-02-01 14:49:27 -0800

    checkpoint1

--- ArrayTests.java
+++ ArrayTests.java
@@ -8,11 +8,22 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
-
+  @Test
+  public void testReverseInPlace1() {
+    int[] input1 = {1,2,3,4,5};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5,4,3,2,1},input1);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    
+  }
+  @Test
+  public void testReversed1() {
+    int[] input1 = {1,2,3,4,5};
+    assertArrayEquals(new int[]{5,4,3,2,1},ArrayExamples.reversed(input1));
   }
 }
---
+++ ListTests.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ListTests {
+    @Test
+    public void testFilter() {
+        List<String> testList= new List <String>();
+
+
+    }
+}
